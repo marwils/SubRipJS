55ec3bbc00c8b6d2df2c069a58cf3fc07e031d90
Martin Wilsdorf <de.marwils@gmail.com>
2016-10-07 09:44:55 +0200

    Bugfix, Class optimization
    Merged SubRipObject class and SubRip class.
    Fixed a bug in append method.

--- subrip.js
+++ subrip.js
@@ -5,51 +5,59 @@ strip = function(s) {
 	return s.replace(/^\s+|\s+$/g, '');
 },
 
-SubRip = {
-	parse: function(subRipString) {
-		var
-		entries,
-		entry, entryIndex, entryLines, entryLinesLength, lineIndex,
-		entryData, timeData, textData,
-		subRipObject = new SubRipObject();
-
-		subRipString = strip(subRipString.replace(/\r\n|\r|\n/g, '\n'));
-		entries = subRipString.split('\n\n');
-
-		for (entryIndex in entries) {
-			entry = entries[entryIndex];
-			entryLines = entry.split('\n');
-
-			if (entryLines.length < 2) {
-				continue;
-			}
-
-			timeData = entryLines[1].split(' --> ');
-			textData = entryLines[2];
-
-			for (lineIndex = 3, entryLinesLength = entryLines.length; lineIndex < entryLinesLength; textData += '\n' + entryLines[lineIndex++]);
-
-			subRipObject.append(new SubRipEntryData(entryLines[0], strip(timeData[0]), strip(timeData[1]), textData));
-		}
-
-		return subRipObject;
-	}
+SubRip = function() {
+	this.entries = [];
 },
 
 SubRipTimeObject = function(timeData) {
 	var
 	date = new Date(0),
 	timeSplit = timeData.split(','),
-	upperTime = timeSplit[0].split(':'),
+	timeHigh = timeSplit[0].split(':'),
 	millis = timeSplit[1];
 
-	date.setHours(upperTime[0]);
-	date.setMinutes(upperTime[1]);
-	date.setSeconds(upperTime[2]);
+	date.setHours(timeHigh[0]);
+	date.setMinutes(timeHigh[1]);
+	date.setSeconds(timeHigh[2]);
 	date.setMilliseconds(millis);
 
 	this.text = timeData;
 	this.date = date;
+};
+
+SubRip.prototype.append = function(subRipEntryData) {
+	if (subRipEntryData instanceof SubRipEntryData) {
+		this.entries.push(subRipEntryData);
+	}
+};
+
+SubRip.parse = function(subRipString) {
+	var
+	entries,
+	entry, entryIndex, entryLines, entryLinesLength, lineIndex,
+	entryData, timeData, textData,
+	subRipObject = new SubRip();
+
+	subRipString = strip(subRipString.replace(/\r\n|\r|\n/g, '\n'));
+	entries = subRipString.split('\n\n');
+
+	for (entryIndex in entries) {
+		entry = entries[entryIndex];
+		entryLines = entry.split('\n');
+
+		if (entryLines.length < 2) {
+			continue;
+		}
+
+		timeData = entryLines[1].split(' --> ');
+		textData = entryLines[2];
+
+		for (lineIndex = 3, entryLinesLength = entryLines.length; lineIndex < entryLinesLength; textData += '\n' + entryLines[lineIndex++]);
+
+		subRipObject.append(new SubRipEntryData(entryLines[0], strip(timeData[0]), strip(timeData[1]), textData));
+	}
+
+	return subRipObject;
 };
 
 SubRipTimeObject.prototype.toSeconds = function() {
@@ -65,16 +73,6 @@ SubRipEntryData = function(id, start, end, text) {
 	this.start = new SubRipTimeObject(start);
 	this.end = new SubRipTimeObject(end);
 	this.text = text;
-};
-
-SubRipObject = function() {
-	this.entries = [];
-};
-
-SubRipObject.prototype.append = function(subRipEntryData) {
-	if (typeofsubRipEntryData instanceof SubRipEntryData) {
-		this.entries.push(subRipEntryData);
-	}
 };
 
 window.SubRip = SubRip;
